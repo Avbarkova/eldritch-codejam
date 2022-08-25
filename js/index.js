af2731cc41cba66d36c6b2272a1b235b0959ff44
Avbarkova <avbarkova@gmail.com>
2022-08-25 20:14:19 +0300

    docs: create html, js, css files

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/*import * as Ancients from "../codejam/data/ancients.js"
+import * as Blue from "../codejam/data/mythicCards/blue/index.js"
+import * as Brown from "../codejam/data/mythicCards/brown/index.js";
+import * as Green from "../codejam/data/mythicCards/green/index.js";*/
+
+import ancients from '../assets/Ancients/index.js';
+console.log(ancients);
